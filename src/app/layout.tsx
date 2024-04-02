a09bad503e2ee7c2f77890917d9bbfc190a5ef09
Jakelambert1 <jake.lambert@anmut.co.uk>
2024-04-02 09:07:52 +0100

    Changed the Font and added padding around website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { ColorSchemeScript, createTheme, MantineProvider } from "@mantine/core";
-import { Montserrat } from "next/font/google";
+import { Roboto_Condensed } from "next/font/google";
 import "@mantine/core/styles.css";
+import NavigationBar from "./NavigationBar";
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const montserrat = Roboto_Condensed({ subsets: ["greek"], weight: ["400"] });
 
 export const metadata: Metadata = {
   title: "Daniella Makeup",
@@ -24,8 +25,11 @@ export default function RootLayout({
       <head>
         <ColorSchemeScript />
       </head>
-      <body className={montserrat.className}>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+      <body className={montserrat.className} style={{ padding: "0px 20px 0 20px" }}>
+        <MantineProvider theme={theme}>
+          <NavigationBar />
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
