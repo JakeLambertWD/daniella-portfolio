958e70c98dd042f54564c9ee68d0a358c2c5748f
Jakelambert1 <jake.lambert@anmut.co.uk>
2024-04-02 09:08:04 +0100

    Remove Appshell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,6 @@
-"use client";
-
-import { AppShell, Burger } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import { LandingPage } from "./LandingPage";
+import LandingPage from "./LandingPage";
 
 function Home() {
-  const [opened, { toggle }] = useDisclosure();
-
-  return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: "sm",
-        collapsed: { mobile: !opened },
-      }}
-      padding="md"
-    >
-      <AppShell.Header>
-        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-        <div>Logo</div>
-      </AppShell.Header>
-
-      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
-
-      <AppShell.Main>
-        <LandingPage />
-      </AppShell.Main>
-    </AppShell>
-  );
+  return <LandingPage />;
 }
 export default Home;
