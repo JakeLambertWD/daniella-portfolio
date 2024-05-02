596599e92ce457c0baaa5c637dd3d099d5f7dc96
Jakelambert1 <jake.lambert@anmut.co.uk>
2024-05-02 18:44:52 +0100

    Add Components to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
-import LandingPage from "./LandingPage";
+import About from "./components/About";
+import ContactMe from "./components/ContactMe";
+import LandingPage from "./components/Landing";
+import Portfolio from "./components/Portfolio";
+import Reviews from "./components/Reviews";
+import Services from "./components/Services";
 
 function Home() {
-  return <LandingPage />;
+  return (
+    <>
+      <LandingPage />
+      <About />
+      <Reviews />
+      <Services />
+      <Portfolio />
+      <ContactMe />
+    </>
+  );
 }
 export default Home;
