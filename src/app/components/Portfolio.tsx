de10ca2d0ed1220efe624e57f23098e493a25fd0
Jakelambert1 <jake.lambert@anmut.co.uk>
2024-05-02 18:46:15 +0100

    Portfolio updates

---
+++ src/app/components/Portfolio.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { Image, Stack } from "@mantine/core";
+import Heading from "./Heading";
+
+function Portfolio() {
+  // TODO: import images from instagram??
+  // TODO: decrease padding between images on small screens
+  // TODO: add hover effect to images
+  // TODO: add modal to images
+  // https://www.w3schools.com/howto/howto_css_image_overlay.asp
+  // TODO: add lazy loading to images
+  // TODO: add infinite scroll to images
+  // TODO: reduce column count on small screens
+  // TODO: Split gallery into a grid with sub sections for each category
+  // TODO: Discuss with her, does she want pics displayed from insta or from her phone?
+
+  return (
+    <Stack align="center" mb={20} bg="rgba(216, 182, 182)">
+      <Heading title="PORTFOLIO" />
+
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          overflow: "auto",
+          height: "800px",
+          borderTop: "8px solid #fff8ff",
+          borderBottom: "8px solid #fff8ff",
+          padding: "0 4px 8px 4px",
+          maxWidth: "1200px",
+          background: "#fff8ff",
+          marginBottom: "70px",
+          marginRight: "20px",
+          marginLeft: "20px",
+          scrollbarWidth: "none",
+        }}
+      >
+        <div style={{ flex: "33.3%", maxWidth: "33.3%", padding: "0 4px" }}>
+          <Image src="/pic15.jpg" alt="alt" style={{ width: "100%", verticalAlign: "middle" }} />
+          <Image src="/pic1.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic3.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic2.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic4.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic20.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic13.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+        </div>
+
+        <div style={{ flex: "33.3%", maxWidth: "33.3%", padding: "0 4px" }}>
+          <Image src="/pic5.jpg" alt="alt" style={{ width: "100%", verticalAlign: "middle" }} />
+          <Image src="/pic6.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic7.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic8.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic19.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic14.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic16.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+        </div>
+
+        <div style={{ flex: "33.3%", maxWidth: "33.3%", padding: "0 4px" }}>
+          <Image src="/pic10.jpg" alt="alt" style={{ width: "100%", verticalAlign: "middle" }} />
+          <Image src="/pic9.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic11.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic12.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic18.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic17.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+          <Image src="/pic21.jpg" alt="alt" style={{ width: "100%", marginTop: "8px", verticalAlign: "middle" }} />
+        </div>
+
+        <div style={{ flex: "25%", maxWidth: "25%", padding: "0 4px" }}></div>
+      </div>
+    </Stack>
+  );
+}
+
+export default Portfolio;
