ec466d0135cb6572ade830b3d1a1c037f4e90993
Jakelambert1 <jake.lambert@anmut.co.uk>
2024-05-02 18:42:26 +0100

    Make Social Links sticky to the right of the screen

--- src/app/SocialMediaLinks.tsx
+++ src/app/SocialMediaLinks.tsx
@@ -1,10 +1,22 @@
 import { Stack } from "@mantine/core";
 import { IconBrandInstagram, IconBrandFacebook } from "@tabler/icons-react";
 
-// TODO: Think about where tro put this on small screens
+// TODO: Think about where to put this on small screens
 function SocialMediaLinks() {
   return (
-    <Stack pos="absolute" right={20} top="50%" align="center" gap="lg" justify="center" py="md" px="md" bg="white">
+    <Stack
+      pos="fixed"
+      w="fit-content"
+      right={20}
+      top="50%"
+      align="center"
+      gap="lg"
+      justify="center"
+      py="md"
+      px="md"
+      bg="white"
+    >
+      {/* TODO: Add links to social media */}
       <IconBrandInstagram size={24} />
       <IconBrandFacebook size={24} />
     </Stack>
